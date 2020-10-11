838a688ac2fa854ef79532cbc64a6bc394bd8c5a
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2020-10-11 15:19:35 +0000

    Improving README.

--- contracts/eosio.boot/include/eosio.boot/eosio.boot.hpp
+++ contracts/eosio.boot/include/eosio.boot/eosio.boot.hpp
@@ -27,4 +27,38 @@ namespace eosioboot {
 
       // explicit serialization macro is not necessary, used here only to improve compilation time
       EOSLIB_SERIALIZE( permission_level_weight, (permission)(weight) )
- 
+   };
+
+   /**
+    * Weighted key.
+    *
+    * @details A weighted key is defined by a public key and an associated weight.
+    */
+   struct key_weight {
+      eosio::public_key  key;
+      uint16_t           weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( key_weight, (key)(weight) )
+   };
+
+   /**
+    * Wait weight.
+    *
+    * @details A wait weight is defined by a number of seconds to wait for and a weight.
+    */
+   struct wait_weight {
+      uint32_t           wait_sec;
+      uint16_t           weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( wait_weight, (wait_sec)(weight) )
+   };
+
+   /**
+    * Blockchain authority.
+    *
+    * @details An authority is defined by:
+    * - a vector of key_weights (a key_weight is a public key plus a weight),
+    * - a vector of permission_level_weights, (a permission_level is an account name plus a permission name)
+    * - a vector of wait_weights (a wait_weight is defined by a
