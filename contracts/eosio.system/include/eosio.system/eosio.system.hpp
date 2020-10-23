c920f6ce5c15b4fc8242ff0015024b1b8f009599
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2020-10-23 14:23:07 +0000

    Added instructions to README

--- contracts/eosio.system/include/eosio.system/eosio.system.hpp
+++ contracts/eosio.system/include/eosio.system/eosio.system.hpp
@@ -23,4 +23,38 @@
 #undef CHANNEL_RAM_AND_NAMEBID_FEES_TO_REX
 #endif
 // CHANNEL_RAM_AND_NAMEBID_FEES_TO_REX macro determines whether ramfee and namebid proceeds are
-// channeled to REX pool. In order to stop these proceeds from being chann
+// channeled to REX pool. In order to stop these proceeds from being channeled, the macro must
+// be set to 0.
+#define CHANNEL_RAM_AND_NAMEBID_FEES_TO_REX 1
+
+namespace eosiosystem {
+
+   using eosio::asset;
+   using eosio::binary_extension;
+   using eosio::block_timestamp;
+   using eosio::check;
+   using eosio::const_mem_fun;
+   using eosio::datastream;
+   using eosio::indexed_by;
+   using eosio::name;
+   using eosio::same_payer;
+   using eosio::symbol;
+   using eosio::symbol_code;
+   using eosio::time_point;
+   using eosio::time_point_sec;
+   using eosio::unsigned_int;
+   // TELOS
+   using producer_location_pair = std::pair<eosio::producer_authority, uint16_t>;
+
+   inline constexpr int64_t powerup_frac = 1'000'000'000'000'000ll;  // 1.0 = 10^15
+
+   template<typename E, typename F>
+   static inline auto has_field( F flags, E field )
+   -> std::enable_if_t< std::is_integral_v<F> && std::is_unsigned_v<F> &&
+                        std::is_enum_v<E> && std::is_same_v< F, std::underlying_type_t<E> >, bool>
+   {
+      return ( (flags & static_cast<F>(field)) != 0 );
+   }
+
+   template<typename E, typename F>
+   static inline auto 
