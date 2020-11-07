7dddbbed50ea6616d8b8b0853462f5f12979910e
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2020-11-07 08:19:35 +0000

    Dev

--- contracts/eosio.system/include/eosio.system/eosio.system.hpp
+++ contracts/eosio.system/include/eosio.system/eosio.system.hpp
@@ -437,4 +437,30 @@ namespace eosiosystem {
       int64_t             staked = 0;
 
       // TELOS BEGIN
-      int64_t             last_
+      int64_t             last_stake = 0;
+      // TELOS END
+
+      //  Every time a vote is cast we must first "undo" the last vote weight, before casting the
+      //  new vote weight.  Vote weight is calculated as:
+      //  stated.amount * 2 ^ ( weeks_since_launch/weeks_per_year)
+      double              last_vote_weight = 0; /// the vote weight cast the last time the vote was updated
+
+      // Total vote weight delegated to this voter.
+      double              proxied_vote_weight= 0; /// the total vote weight delegated to this voter as a proxy
+      bool                is_proxy = 0; /// whether the voter is a proxy for others
+
+
+      uint32_t            flags1 = 0;
+      uint32_t            reserved2 = 0;
+      eosio::asset        reserved3;
+
+      uint64_t primary_key()const { return owner.value; }
+
+      enum class flags1_fields : uint32_t {
+         ram_managed = 1,
+         net_managed = 2,
+         cpu_managed = 4
+      };
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(staked)(last_stake)(las
