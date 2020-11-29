1fc2cbd9901f625caba796a40fcc2a7d0c06f97d
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2020-11-29 14:22:20 +0000

    czp

--- contracts/eosio.system/include/eosio.system/eosio.system.hpp
+++ contracts/eosio.system/include/eosio.system/eosio.system.hpp
@@ -801,4 +801,15 @@ namespace eosiosystem {
 
       uint64_t primary_key()const { return id; }
       uint64_t by_owner()const    { return owner.value; }
-      
+      uint64_t by_expires()const  { return expires.utc_seconds; }
+   };
+
+   typedef eosio::multi_index< "powup.order"_n, powerup_order,
+                               indexed_by<"byowner"_n, const_mem_fun<powerup_order, uint64_t, &powerup_order::by_owner>>,
+                               indexed_by<"byexpires"_n, const_mem_fun<powerup_order, uint64_t, &powerup_order::by_expires>>
+                               > powerup_order_table;
+
+   /**
+    * The `eosio.system` smart contract is provided by `block.one` as a sample system contract, and it defines the structures and actions needed for blockchain's core functionality.
+    *
+    * Just like in the `eosio.bios` sample contract implementation, there are a few actions which are not implemented at the contract level (`newaccount`, `updateauth`, `deleteauth`, `linkauth`, `unlinkauth`, `canceldelay`, `onerror`, `setabi`, `setcode`), they are just declared in the contract so they will show in the contract's ABI and users will be able to push those actions to the chain via the account holding the `eosio.system` contract, but the implementation is at the EOSIO core level. They are referred to as E
