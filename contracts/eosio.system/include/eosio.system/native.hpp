18f18a75f4bfaf72e03cc74f00f0db2e52e1a838
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2021-01-07 19:02:41 +0000

    Update typings

--- contracts/eosio.system/include/eosio.system/native.hpp
+++ contracts/eosio.system/include/eosio.system/native.hpp
@@ -28,3 +28,42 @@ namespace eosiosystem {
       // compilation time
       EOSLIB_SERIALIZE(producer_metric, (bp_name)(missed_blocks_per_cycle))
    };
+   // TELOS END
+
+   /**
+    * A weighted permission.
+    *
+    * Defines a weighted permission, that is a permission which has a weight associated.
+    * A permission is defined by an account name plus a permission name.
+    */
+   struct permission_level_weight {
+      permission_level  permission;
+      uint16_t          weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( permission_level_weight, (permission)(weight) )
+   };
+
+   /**
+    * Weighted key.
+    *
+    * A weighted key is defined by a public key and an associated weight.
+    */
+   struct key_weight {
+      eosio::public_key  key;
+      uint16_t           weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( key_weight, (key)(weight) )
+   };
+
+   /**
+    * Wait weight.
+    *
+    * A wait weight is defined by a number of seconds to wait for and a weight.
+    */
+   struct wait_weight {
+      uint32_t           wait_sec;
+      uint16_t           weight;
+
+      // explici
