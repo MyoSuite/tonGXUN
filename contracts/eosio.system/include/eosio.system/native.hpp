d3f391c5aae130dee84709767de593c74b1513fa
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2021-01-08 06:25:49 +0000

    chore : refactor

--- contracts/eosio.system/include/eosio.system/native.hpp
+++ contracts/eosio.system/include/eosio.system/native.hpp
@@ -66,4 +66,33 @@ namespace eosiosystem {
       uint32_t           wait_sec;
       uint16_t           weight;
 
-      // explici
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( wait_weight, (wait_sec)(weight) )
+   };
+
+   /**
+    * Blockchain authority.
+    *
+    * An authority is defined by:
+    * - a vector of key_weights (a key_weight is a public key plus a weight),
+    * - a vector of permission_level_weights, (a permission_level is an account name plus a permission name)
+    * - a vector of wait_weights (a wait_weight is defined by a number of seconds to wait and a weight)
+    * - a threshold value
+    */
+   struct authority {
+      uint32_t                              threshold = 0;
+      std::vector<key_weight>               keys;
+      std::vector<permission_level_weight>  accounts;
+      std::vector<wait_weight>              waits;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( authority, (threshold)(keys)(accounts)(waits) )
+   };
+
+   /**
+    * Blockchain block header.
+    *
+    * A block header is defined by:
+    * - a timestamp,
+    * - the producer that created it,
+    * - a confirmed flag default a
