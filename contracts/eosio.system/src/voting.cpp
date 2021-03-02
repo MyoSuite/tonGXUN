c98d15a9ab9567fa7ad73bf3420ac2da8a6ee135
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2021-03-02 09:37:48 +0000

    super

--- contracts/eosio.system/src/voting.cpp
+++ contracts/eosio.system/src/voting.cpp
@@ -13,4 +13,37 @@
 #include <type_traits>
 #include <limits>
 #include <set>
-#
+#include <algorithm>
+#include <cmath>
+
+// TELOS BEGIN
+#include <boost/container/flat_map.hpp>
+#include "system_rotation.cpp"
+// TELOS END
+
+namespace eosiosystem {
+
+   using eosio::const_mem_fun;
+   using eosio::current_time_point;
+   using eosio::indexed_by;
+   using eosio::microseconds;
+   using eosio::singleton;
+
+   void system_contract::register_producer( const name& producer, const eosio::block_signing_authority& producer_authority, const std::string& url, uint16_t location ) {
+      auto prod = _producers.find( producer.value );
+      const auto ct = current_time_point();
+
+      eosio::public_key producer_key{};
+
+      std::visit( [&](auto&& auth ) {
+         if( auth.keys.size() == 1 ) {
+            // if the producer_authority consists of a single key, use that key in the legacy producer_key field
+            producer_key = auth.keys[0].key;
+         }
+      }, producer_authority );
+
+      if ( prod != _producers.end() ) {
+         _producers.modify( prod, producer, [&]( producer_info& info ){
+            info.producer_key       = producer_key;
+            info.is_active          = true;
+            info.url               
