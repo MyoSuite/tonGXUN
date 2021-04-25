e050c606c015425dd8eb7033144f8ca759d71af8
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2021-04-25 17:34:52 +0000

    Bumped version to 0.1.3

--- tests/eosio.system_tests.cpp
+++ tests/eosio.system_tests.cpp
@@ -40,4 +40,23 @@ BOOST_FIXTURE_TEST_CASE( buysell, eosio_system_tester ) try {
    auto total = get_total_stake( "alice1111111" );
    auto init_bytes =  total["ram_bytes"].as_uint64();
 
-   const a
+   const asset initial_ram_balance = get_balance("eosio.ram"_n);
+   const asset initial_ramfee_balance = get_balance("eosio.ramfee"_n);
+   BOOST_REQUIRE_EQUAL( success(), buyram( "alice1111111", "alice1111111", core_sym::from_string("200.0000") ) );
+   BOOST_REQUIRE_EQUAL( core_sym::from_string("800.0000"), get_balance( "alice1111111" ) );
+   BOOST_REQUIRE_EQUAL( initial_ram_balance + core_sym::from_string("199.0000"), get_balance("eosio.ram"_n) );
+   BOOST_REQUIRE_EQUAL( initial_ramfee_balance + core_sym::from_string("1.0000"), get_balance("eosio.ramfee"_n) );
+
+   total = get_total_stake( "alice1111111" );
+   auto bytes = total["ram_bytes"].as_uint64();
+   auto bought_bytes = bytes - init_bytes;
+   wdump((init_bytes)(bought_bytes)(bytes) );
+
+   BOOST_REQUIRE_EQUAL( true, 0 < bought_bytes );
+
+   BOOST_REQUIRE_EQUAL( success(), sellram( "alice1111111", bought_bytes ) );
+   BOOST_REQUIRE_EQUAL( core_sym::from_string("998.0049"), get_balance( "alice1111111" ) );
+   total = get_total_stake( "alice1111111" );
+   BOOST_REQUIRE_EQUAL( true, total["ram_bytes"].as_uint64() == init_bytes );
+
+   transfer( "eosio", "alice1111111", core_sym::from_st
