a9946f60273e74358176161a2a18cbe4dc7b9ed0
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2022-06-09 06:40:59 +0000

    Squash migrations

--- tests/eosio.system_tests.cpp
+++ tests/eosio.system_tests.cpp
@@ -3740,4 +3740,26 @@ BOOST_FIXTURE_TEST_CASE( wasmcfg, eosio_system_tester ) try {
    {
       fc::variant pretty_trx = fc::mutable_variant_object()
          ("expiration", "2020-01-01T00:30")
-         ("ref_bl
+         ("ref_block_num", 2)
+         ("ref_block_prefix", 3)
+         ("net_usage_words", 0)
+         ("max_cpu_usage_ms", 0)
+         ("delay_sec", 0)
+         ("actions", fc::variants({
+               fc::mutable_variant_object()
+                  ("account", name(config::system_account_name))
+                  ("name", "wasmcfg")
+                  ("authorization", vector<permission_level>{ { config::system_account_name, config::active_name } })
+                  ("data", fc::mutable_variant_object()
+                   ("settings", "high"_n)
+                  )
+                  })
+         );
+      abi_serializer::from_variant(pretty_trx, trx, get_resolver(), abi_serializer::create_yield_function(abi_serializer_max_time));
+   }
+
+   BOOST_REQUIRE_EQUAL(success(), push_action_msig( "alice1111111"_n, "propose"_n, mvo()
+                                                    ("proposer",      "alice1111111")
+                                                    ("proposal_name", "setparams1")
+                                                    ("trx",           trx)
+                                                  
