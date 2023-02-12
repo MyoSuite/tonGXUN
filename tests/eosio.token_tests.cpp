689de64331ab2fc8decd9bc6e3e3f78adb3e1bcc
MyoSuite <106216799+MyoSuite@users.noreply.github.com>
2023-02-12 16:46:48 +0000

    Error handling improvements

--- tests/eosio.token_tests.cpp
+++ tests/eosio.token_tests.cpp
@@ -31,4 +31,28 @@ public:
       produce_blocks();
 
       const auto& accnt = control->db().get<account_object,by_name>( "eosio.token"_n );
-      abi_def ab
+      abi_def abi;
+      BOOST_REQUIRE_EQUAL(abi_serializer::to_abi(accnt.abi, abi), true);
+      abi_ser.set_abi(abi, abi_serializer::create_yield_function(abi_serializer_max_time));
+   }
+
+   action_result push_action( const account_name& signer, const action_name &name, const variant_object &data ) {
+      string action_type_name = abi_ser.get_action_type(name);
+
+      action act;
+      act.account = "eosio.token"_n;
+      act.name    = name;
+      act.data    = abi_ser.variant_to_binary( action_type_name, data, abi_serializer::create_yield_function(abi_serializer_max_time) );
+
+      return base_tester::push_action( std::move(act), signer.to_uint64_t() );
+   }
+
+   fc::variant get_stats( const string& symbolname )
+   {
+      auto symb = eosio::chain::symbol::from_string(symbolname);
+      auto symbol_code = symb.to_symbol_code().value;
+      vector<char> data = get_row_by_account( "eosio.token"_n, name(symbol_code), "stat"_n, account_name(symbol_code) );
+      return data.empty() ? fc::variant() : abi_ser.binary_to_variant( "currency_stats", data, abi_serializer::create_yield_function(abi_serializer_max_time) );
+   }
+
+   
